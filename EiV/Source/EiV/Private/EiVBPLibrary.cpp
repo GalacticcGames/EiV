376d8724f744ed8b3fd2a37f5992bef042daa703
GalacticcGames <137231823+GalacticcGames@users.noreply.github.com>
2025-04-06 13:56:23 -0600

    Changing License info and date

--- EiV/Source/EiV/Private/EiVBPLibrary.cpp
+++ EiV/Source/EiV/Private/EiVBPLibrary.cpp
@@ -1,9 +1,9 @@
 // Copyright 2025, Galacticc Games. All rights reserved.
 
-/* Licensed under MIT license. See License.txt for full license text.
+/* Licensed under MIT license. See LICENSE for full license text.
 *
 *        Created: 20th March 2025
-*  Last Modified: 20th March 2025
+*  Last Modified: 6th April 2025
 */
 
 #include "EiVBPLibrary.h"
--- EiV/Source/EiV/Private/EiVLibrary.cpp
+++ EiV/Source/EiV/Private/EiVLibrary.cpp
@@ -1,8 +1,8 @@
 // Copyright 2025, Galacticc Games. All rights reserved.
 
-/* Licensed under MIT license. See License.txt for full license text.
+/* Licensed under MIT license. See LICENSE for full license text.
 *
 *        Created: 20th March 2025
-*  Last Modified: 20th March 2025
+*  Last Modified: 6th April 2025
 */
 #include "EiVLibrary.h"
