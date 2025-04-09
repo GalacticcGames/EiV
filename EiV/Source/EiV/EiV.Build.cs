b0409812010511524ddce56a143bc0a1e45fa85a
GalacticcGames <137231823+GalacticcGames@users.noreply.github.com>
2025-04-08 18:27:00 -0600

    Add url key to .uplugin

--- EiV/Source/EiV/EiV.Build.cs
+++ EiV/Source/EiV/EiV.Build.cs
@@ -3,7 +3,7 @@
 /* Licensed under MIT license. See LICENSE for full license text.
 *
 *        Created: 20th March 2025
-*  Last Modified: 6th April 2025
+*  Last Modified: 8th April 2025
 */
 
 using UnrealBuildTool;
@@ -18,7 +18,7 @@ public class EiV : ModuleRules
 			
 		PrivateDependencyModuleNames.AddRange(new string[] { "CoreUObject", "Engine", "Eigen" });
 
-        AddEngineThirdPartyPrivateStaticDependencies(Target, "Eigen");
-        PublicDefinitions.Add("EIGEN_IGNORE_UNREACHABLE_CODE_WARNING=1");
-    }
+		AddEngineThirdPartyPrivateStaticDependencies(Target, "Eigen");
+		PublicDefinitions.Add("EIGEN_IGNORE_UNREACHABLE_CODE_WARNING=1");
+	}
 }
